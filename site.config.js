7e8fc11a7d54ec9f3c1bfbbdacbda417700aacfd
yujin <127318207+sophia22001@users.noreply.github.com>
2025-04-09 02:54:44 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -56,7 +56,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO=sophia22001/morethan-log || "",
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
